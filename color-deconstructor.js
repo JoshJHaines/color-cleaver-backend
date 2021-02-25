87eaa8ff5fa25794693431b1eca25178f1c9d8dd
JoshuaJHaines <joshjhaines3@gmail.com>
2021-02-25 11:51:50 -0500

    created 'colorDeconstructor' function. tests passed

--- color-deconstructor.js
+++ color-deconstructor.js
@@ -2,13 +2,20 @@
  * YOUR CODE HERE *
  ******************/
 
-
-
-
-// Our setup code here. Don't touch!
-if(typeof colorDeconstructor === 'undefined') {
-  colorDeconstructor = undefined;
+function colorDeconstructor(color) {
+	if (color === "orange") {
+		return "red and yellow";
+	}
+	if (color === "green") {
+		return "blue and yellow";
+	} else {
+		return "red and blue";
+	}
 }
 
+// Our setup code here. Don't touch!
+if (typeof colorDeconstructor === "undefined") {
+	colorDeconstructor = undefined;
+}
 
 module.exports = colorDeconstructor;
