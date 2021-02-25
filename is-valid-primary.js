21b8453d61c6d50a506ef43d9292c7e93db93e54
JoshuaJHaines <joshjhaines3@gmail.com>
2021-02-25 11:45:33 -0500

    created 'isValidPrimary' function. passed tests

--- is-valid-primary.js
+++ is-valid-primary.js
@@ -2,12 +2,13 @@
  * YOUR CODE HERE *
  ******************/
 
-
-
+function isValidPrimary(color) {
+	return color === "red" || color === "blue" || color === "yellow";
+}
 
 // Our setup code here. Don't touch!
-if(typeof isValidPrimary === 'undefined') {
-  isValidPrimary = undefined;
+if (typeof isValidPrimary === "undefined") {
+	isValidPrimary = undefined;
 }
 
 module.exports = isValidPrimary;
